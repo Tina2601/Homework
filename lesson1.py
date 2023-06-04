a7e41995b4b32087aaaaa3d11fa334023f092aba
Tina2601 <valen.tina2624@outlook.com>
2023-06-04 16:12:59 +0300

    Solution

---
+++ lesson1.py
@@ -0,0 +1,15 @@
+n = int(input("How many students?  :"))
+k = int(input("How many apples?  :"))
+print(int(k) // (n))
+print(int(k) % (n))
+
+
+a = int(input("How many students are in total?:  "))
+b = int((a) / 3)
+c = int((b) / 2)
+d = int((c) * 3)
+print(int(b),(c),(d))
+# or open answer:
+print("Nun of students in every class:" + str(b), "Desks in every class:" + str(c), "Desks for all 3 classes:" + str(d))
+#question: why the clss 'int' was saved? -
+print((a), type(a))
